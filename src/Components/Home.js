9f88a7104f102b9f6679baeb339b90d65ee4ae70
chetas11 <cbirmole@gmail.com>
2021-03-10 17:18:12 +0530

    complete

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -23,19 +23,23 @@ function Home() {
     return (
         <div className="container">
         <div className="row">
-            <div className="col-lg-4 col-md-4 col-sm-6"></div>
-            <div className="col-lg-4 col-md-4 col-sm-6">
+            <div className="col-lg-4 col-md-6 col-sm-6"></div>
+            <div className="col-lg-4 col-md-12 col-sm-12">
                 <div className="row text-center top-container">
                     <div className="col-lg-6 col-md-6 col-sm-6">
+                        <div className="col-lg-6 col-md-6 col-sm-6 col-2">
                         <button onClick={notify} className="btn btn-warning start mt-5 mb-5"><i class="far fa-play-circle"></i><br />PLAY</button>
-                        <img className="Banner img-fluid" src={Banner} />
+                        </div>
+                        <div className="col-10">
+                            <img className="Banner img-fluid" src={Banner} />
+                        </div>
                     </div>
                     <hr />
-                    <div className="col-lg-6 col-md-6 col-sm-3">
+                    <div className="col-lg-6 col-md-6 col-sm-6 col-6">
                         <button onClick={notify} className="btn custom my-2 p-3"><span><i class="fas fa-user"></i> Custom Game</span></button><br />
                         <button onClick={notify} className="btn online my-2 p-3"><span><i class="fas fa-globe"></i> Game Online</span></button>
                     </div>
-                    <div className="col-lg-6 col-md-6 col-sm-3">
+                    <div className="col-lg-6 col-md-6 col-sm-6 col-6">
                         <button onClick={StartGame} className="btn twop my-2 p-3"><span><i class="fas fa-user-friends"></i> Two Players</span></button><br />
                         <button onClick={notify} className="btn training my-2 p-3"><span><i class="fas fa-brain"></i> Training Game</span></button>
                     </div>
