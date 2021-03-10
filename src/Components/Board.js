795a325538b149abbaea0fa5fa2bae554b9733ab
chetas11 <cbirmole@gmail.com>
2021-03-10 18:27:38 +0530

    added styles

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,19 +2,9 @@ import React from 'react'
 import Square from './Square'
 
 
-const style = {
-    border: '4px solid #6e7c7c',
-    borderRadius: '10px',
-    width:'350px',
-    height:'350px',
-    margin: '100px auto 0px auto',
-    display: 'grid',
-    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
-}
-
 function Board({squares, onClick}) {
     return (
-        <div style={style}>
+        <div className="board">
             {squares.map((square, i)=> (
                 <Square key={i} value={square} onClick={()=>onClick(i)} />
             ))}
